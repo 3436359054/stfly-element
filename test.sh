392771124474e1d62966b3de4703eb0ec9effe7d
“suntianfeng” <s18917634009@163.com>
2024-07-12 14:06:07 +0800

    dev-20240713

--- test.sh
+++ test.sh
@@ -16,31 +16,32 @@ ERROR_ICON="[✗]"
 
 # 获取当前日期，格式化为YYYYMMDD
 current_date=$(date +%Y%m%d)
-target_branch="master-$current_date"
+target_branch="master-$current_date" # 目标分支
+current_branch=$(git rev-parse --abbrev-ref HEAD) # 当前分支
 
 # 检查是否有未提交的更改
 if git status --porcelain | grep -q .; then
     echo "${INFO_ICON}${BLUE} 发现未提交的更改在当前分支。${NC}"
     git add .
     
-    read -p "${CYAN}请输入提交信息（当前分支）: ${NC}" commit_message
+    # read -p "${CYAN}请输入提交信息（当前分支）: ${NC}" commit_message
     
     # 执行提交
-    git commit -m "$commit_message"
+    git commit -m "$current_branch"
     
     # 推送提交到远程仓库
-    echo "${INFO_ICON}${BLUE} 正在推送当前分支到远程仓库...${NC}"
-    git push $(git rev-parse --abbrev-ref HEAD)
-    echo "${SUCCESS_ICON}${GREEN} 当前分支已成功提交并推送至远程仓库。${NC}"
+    echo "${INFO_ICON}${BLUE} 正在推送当前分支($current_branch)到远程仓库...${NC}"
+    git push
+    echo "${SUCCESS_ICON}${GREEN} 当前分支($current_branch)已成功提交并推送至远程仓库。${NC}"
 else
-    echo "${WARNING_ICON}${YELLOW} 当前分支上没有更改需要提交。${NC}"
+    echo "${WARNING_ICON}${YELLOW} 当前分支($current_branch)上没有更改需要提交。${NC}"
 fi
 
 # 检查目标分支是否已经存在
 if git show-ref --verify --quiet "refs/heads/$target_branch"; then
     echo "${INFO_ICON}${BLUE} 目标分支 ${target_branch} 已存在，正在切换并拉取最新代码。${NC}"
     git checkout $target_branch
-    git pull $target_branch
+    git pull
 else
     echo "${INFO_ICON}${BLUE} 目标分支 ${target_branch} 不存在，正在创建。${NC}"
     git checkout -b $target_branch
@@ -49,8 +50,8 @@ else
 fi
 
 # 合并当前分支到目标分支
-echo "${INFO_ICON}${BLUE} 正在合并当前分支到 ${target_branch}...${NC}"
-git merge $(git rev-parse --abbrev-ref HEAD) -m "合并当前分支至 ${target_branch}"
+echo "${INFO_ICON}${BLUE} 正在将${current_branch}分支合并当前分支到 ${target_branch}...${NC}"
+git merge $current_branch
 
 # 检查合并是否有冲突
 if git diff --name-only --diff-filter=U; then
@@ -59,11 +60,11 @@ if git diff --name-only --diff-filter=U; then
 fi
 
 # 提交合并结果
-git add .
-read -p "${CYAN}请输入合并提交的信息: ${NC}" merge_commit_message
-git commit -m "$merge_commit_message"
+# git add .
+# read -p "${CYAN}请输入合并提交的信息: ${NC}" merge_commit_message
+# git commit -m "$merge_commit_message"
 
 # 推送合并结果到远程仓库
 echo "${INFO_ICON}${BLUE} 正在推送合并结果到远程仓库...${NC}"
-git push $target_branch
+git push
 echo "${SUCCESS_ICON}${GREEN} 合并结果已成功推送至远程仓库。${NC}"
