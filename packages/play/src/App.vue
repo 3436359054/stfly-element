1ebe794cf9fd63ef338c1dcbe39a7a9cebf77ccd
“suntianfeng” <s18917634009@163.com>
2024-07-16 16:37:48 +0800

    demo1

--- packages/play/src/App.vue
+++ packages/play/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 </script>
 <template>
-  <fly-button type="primary" size="small">测试1下</fly-button>
+  <fly-button type="primary" size="small">测试下</fly-button>
 
 </template>
