8e51a67249f641cd9d85d1ad412741fcd3622db0
“suntianfeng” <s18917634009@163.com>
2024-07-17 13:37:22 +0800

    增加脚本

--- demo11.sh
+++ demo11.sh
@@ -2,7 +2,7 @@ ERROR_ICON='✗'
 RED='\033[0;31m'
 NC='\033[0m'
 
-if git push origin main 2>&1 | grep -qE 'error'; then
+if git push 2>&1 | grep -qE 'error'; then
     echo "${ERROR_ICON}${RED} 推送失败，请检查网络连接或权限。${NC}"
     exit 1
 fi
