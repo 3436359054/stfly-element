a3197901f4105cc396cd29a713de159a544d42cb
“suntianfeng” <s18917634009@163.com>
2024-07-18 15:19:45 +0800

    demo33

--- packages/play/src/App.vue
+++ packages/play/src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 </script>
 <template>
-  <fly-button type="primary" size="small">测试3</fly-button>
   <fly-button type="primary" size="small">测221</fly-button>
   <fly-button type="primary" size="small">测221</fly-button>
   <fly-button type="primary" size="small">测221</fly-button>
