ae7c4954d52c84959a464839306eb85ef4657ec3
“suntianfeng” <s18917634009@163.com>
2024-10-15 14:53:28 +0800

    打包

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import stflyElement from 'stfly-element';
+import 'stfly-element/dist/index.css';
 
 createApp(App).use(stflyElement).mount('#app')
